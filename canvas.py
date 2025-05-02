a2aaaa95f6ad6a1f934377ee6f75528a50f21e23
Konoe <20051031jason@gmail.com>
2025-05-03 00:16:39 +0800

    The diamond can move now

--- canvas.py
+++ canvas.py
@@ -4,23 +4,34 @@ from player import Player
 from entity import Entity
 
 class Canvas:
+	S_TITLE = 0;	S_SETTING = 1;	S_CREDITS = 2
+	S_BATTLE = 3;	S_SHOP = 4;		S_END = 5
 	def __init__(self):
+		self.stage = Canvas.S_BATTLE
 		self.entities = [Entity() for i in range(3)]
 		self.player = Player()
+		pass
 	def draw_unit(self, screen: pygame.Surface, entity: Player | Entity):
 		width = screen.get_width()
 		height = screen.get_height()
 		center = Vector2(width, height) / 2
-		delta = min((height - 50) / 6, width / 10)
+		# 共 7 條橫線和 11 條 直線，各分 8 塊和 12 塊
+		delta = min((height - 50) / 8, width / 12)
 		shift = Vector2(entity.icon.get_width(), entity.icon.get_height()) / 2
 		position = center + entity.pos * delta - shift
 		screen.blit(entity.icon, position.to_tuple())
 		pass
-	def draw(self, screen: pygame.Surface):
+	def draw_battle(self, screen: pygame.Surface):
 		# fill background
-		screen.fill((127, 127, 255))
+		screen.fill((255, 127, 127))
 		# Draw player
 		self.draw_unit(screen, self.player)
 		# Draw entities
 		for entity in self.entities:
 			self.draw_unit(screen, entity)
+		pass
+	def draw(self, screen: pygame.Surface):
+		if (self.stage == Canvas.S_BATTLE):
+			self.draw_battle(screen)
+	def next_round(self):
+		pass
--- game.py
+++ game.py
@@ -18,6 +18,22 @@ while True:
 		elif event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_ESCAPE:
 				canvas = Canvas()
+			elif event.key in {pygame.K_w, pygame.K_UP}:
+				if canvas.player.pos.y > -3:
+					canvas.player.pos.y -= 1
+					canvas.next_round()
+			elif event.key in {pygame.K_s, pygame.K_DOWN}:
+				if canvas.player.pos.y < 3:
+					canvas.player.pos.y += 1
+					canvas.next_round()
+			elif event.key in {pygame.K_a, pygame.K_LEFT}:
+				if (canvas.player.pos.x > -5):
+					canvas.player.pos.x -= 1
+					canvas.next_round()
+			elif event.key in {pygame.K_d, pygame.K_RIGHT}:
+				if (canvas.player.pos.x < 5):
+					canvas.player.pos.x += 1
+					canvas.next_round()
 			pass
 	
 	
