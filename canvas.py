f979b567e1f97916c10f5139b3148fc70f51d178
Konoe <20051031jason@gmail.com>
2025-05-03 00:45:32 +0800

    Shadows are there!

--- canvas.py
+++ canvas.py
@@ -1,15 +1,29 @@
 import pygame
+import random
 from vector2 import Vector2
 from player import Player
 from entity import Entity
 
 class Canvas:
 	S_TITLE = 0;	S_SETTING = 1;	S_CREDITS = 2
-	S_BATTLE = 3;	S_SHOP = 4;		S_END = 5
+	S_BATTLE = 3;	S_BOSS = 4;		S_SHOP = 5;		S_END = 6
 	def __init__(self):
 		self.stage = Canvas.S_BATTLE
-		self.entities = [Entity() for i in range(3)]
+		self.entities = [Entity(
+			"./resource/image/iron_ingot.png",
+			Entity.T_MOSTER,
+			1,
+			Vector2(j * -5, random.randrange(-3, 3)),
+			Vector2(j, 0)
+		) for j in [random.choice((-1, 1))] for i in range(3)]
 		self.player = Player()
+		self.shadows = [Entity(
+			"./resource/image/emerald.png",
+			Entity.T_SHADOW,
+			-1,
+			Vector2(0, i),
+			Vector2(0, 0)
+		) for i in range(-3, 4)]
 		pass
 	def draw_unit(self, screen: pygame.Surface, entity: Player | Entity):
 		width = screen.get_width()
@@ -26,12 +40,71 @@ class Canvas:
 		screen.fill((255, 127, 127))
 		# Draw player
 		self.draw_unit(screen, self.player)
+		# Draw shadows
+		for shadow in self.shadows:
+			if not shadow.pos.y == self.player.pos.y:
+				self.draw_unit(screen, shadow)
 		# Draw entities
 		for entity in self.entities:
 			self.draw_unit(screen, entity)
 		pass
+	def pressed_battle(self, key):
+		if key in {pygame.K_w, pygame.K_UP}:
+			if self.player.pos.y > -3:
+				self.player.pos.y -= 1
+				for shadow in self.shadows:
+					if self.player.pos.y == shadow.pos.y:
+						self.player.pos.x = shadow.pos.x
+				self.next_round()
+		elif key in {pygame.K_s, pygame.K_DOWN}:
+			if self.player.pos.y < 3:
+				self.player.pos.y += 1
+				for shadow in self.shadows:
+					if self.player.pos.y == shadow.pos.y:
+						self.player.pos.x = shadow.pos.x
+				self.next_round()
+		elif key in {pygame.K_a, pygame.K_LEFT}:
+			if (self.player.pos.x > -5):
+				self.player.pos.x -= 1
+				for shadow in self.shadows:
+					if shadow.pos.y == self.player.pos.y:
+						shadow.pos.x = self.player.pos.x
+				self.next_round()
+		elif key in {pygame.K_d, pygame.K_RIGHT}:
+			if (self.player.pos.x < 5):
+				self.player.pos.x += 1
+				for shadow in self.shadows:
+					if shadow.pos.y == self.player.pos.y:
+						shadow.pos.x = self.player.pos.x
+				self.next_round()
+	def pressed_boss(self, key):
+		if key in {pygame.K_w, pygame.K_UP}:
+			if self.player.pos.y > -3:
+				self.player.pos.y -= 1
+				self.next_round()
+		elif key in {pygame.K_s, pygame.K_DOWN}:
+			if self.player.pos.y < 3:
+				self.player.pos.y += 1
+				self.next_round()
+		elif key in {pygame.K_a, pygame.K_LEFT}:
+			if (self.player.pos.x > -5):
+				self.player.pos.x -= 1
+				self.next_round()
+		elif key in {pygame.K_d, pygame.K_RIGHT}:
+			if (self.player.pos.x < 5):
+				self.player.pos.x += 1
+				self.next_round()
 	def draw(self, screen: pygame.Surface):
 		if (self.stage == Canvas.S_BATTLE):
 			self.draw_battle(screen)
+		elif (self.stage == Canvas.S_BOSS):
+			self.draw_boss(screen)
+	def pressed(self, key: int):
+		if (self.stage == Canvas.S_BATTLE):
+			self.pressed_battle(key)
+		elif (self.stage == Canvas.S_BOSS):
+			self.pressed_boss(key)
 	def next_round(self):
+		for entity in self.entities:
+			entity.next_step()
 		pass
--- entity.py
+++ entity.py
@@ -4,23 +4,20 @@ import random
 from vector2 import Vector2 
 
 class Entity(pygame.sprite.Sprite):
-	def __init__(self):
-		self.type = 0  # type of entity
-		self.pos = Vector2(random.randrange(-5, 5), random.randrange(-3, 3))
-		self.icon = pygame.transform.scale(
-			pygame.image.load("./resource/image/iron_ingot.png"),
-			(100, 100)
-		)
-		self.hp = 0
-		self.x_move_amount = 0  # how long do the entity move
-		self.y_move_amount = 0  # only boss has this
-		self.wait_time = 0  # how many rounds do the entity take to move
+	T_MOSTER = 0;	T_BOSS = 1;	T_SHADOW = 2;	T_SHOP = 3
+	def __init__(self, icon: str, type: int, hp: int, pos: Vector2, mov: Vector2, wait_time: int = 1):
+		super().__init__()
+		self.type = type  # type of entity
+		self.hp = hp
+		self.pos = pos
+		self.icon = pygame.transform.scale(pygame.image.load(icon), (100, 100))
+		self.move = mov # how long do the entity move
+		self.wait_time = wait_time  # how many rounds do the entity take to move
 		self.round_pass = 0  # how many rounds pass
-		def next_step(self): # pass to next position 
-			self.round_pass += 1
-			if self.round_pass >= self.wait_time:
-				self.pos.x += self.x_move_amount
-				self.pos.y += self.y_move_amount
-		def draw(self, screen: pygame.Surface, x: int, y: int):
-			screen.blit(self.icon, (x, y))
-			pass
+	def next_step(self): # pass to next position 
+		self.round_pass += 1
+		if self.round_pass >= self.wait_time:
+			self.pos.x += self.move.x
+			self.pos.y += self.move.y
+			self.round_pass -= self.wait_time
+		pass
